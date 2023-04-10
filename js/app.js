df77af6fdb84e6cbe2a3d5458d8db5377db1a357
Andriikryl <andreyk557@gmail.com>
2023-04-10 20:02:25 +0300

    feat[ navigator.mediaDevices]

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 function main() {
   window.addEventListener("deviceorientation", onOrientationChange);
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then(function (signal) {
+      const video = document.getElementById("myVideo");
+      video.srcObject = signal;
+      video.play();
+    })
+    .catch(function (err) {
+      alert(err);
+    });
 }
 
 function onOrientationChange(event) {
@@ -8,7 +18,9 @@ function onOrientationChange(event) {
     angle = 0;
   }
 
-  const distToTree = 20;
+  const distToTree = document.getElementById("mySlider").value;
+  document.getElementById("myLable").innerHTML =
+    "Distance to tree:" + distToTree + "meters";
   const height = Math.tan((angle * Math.PI) / 180) * distToTree;
   document.getElementById("heightInfo").innerHTML =
     height.toFixed(1) + "m (" + angle.toFixed(1) + "&deg;)";
